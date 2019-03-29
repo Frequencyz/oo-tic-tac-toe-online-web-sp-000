08b95fe7388301569cb1cac14b74c40a452ba3be
Andrew Bay <daddnbay@yahoo.com>
2019-03-29 23:56:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -80,9 +80,9 @@ def won?
   win_index_1 = win_combination[1]
   win_index_2 = win_combination[2]
  
-   position_1 = board[win_index_0]
-   position_2 = board[win_index_1]
-   position_3 = board[win_index_2]
+   position_1 = @board[win_index_0]
+   position_2 = @board[win_index_1]
+   position_3 = @board[win_index_2]
    
    if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return win_combination 
